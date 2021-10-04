8002ad5c4e9d0b2642cbf8e6e6c610a2e2b143c8
Marcos Amorim <markstm10@gmail.com>
2021-10-03 22:13:27 -0300

    🛠 configurado e importado fontes

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -14,6 +14,14 @@ export const GlobalStyle = createGlobalStyle`
         background: var(--background);
         -webkit-font-smoothing: antialiased;
     }
+    body, button{
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, strong{
+        font-weight: 600;
+    }
 
     html{
         @media(max-width: 1080px){
