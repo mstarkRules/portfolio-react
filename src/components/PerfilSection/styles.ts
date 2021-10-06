c2b06b1013fce47788a8c98ad004a136c7af4b50
Marcos Amorim <markstm10@gmail.com>
2021-10-06 15:05:59 -0300

    🛠 trabalhado na estrutura pra inserir o tema. Criado PerfilSection

---
+++ src/components/PerfilSection/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 10px;
+
+  h1 {
+  }
+`;
