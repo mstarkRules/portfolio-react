c3834dc7a1b6d72ff9b49aba7240c974e26a2081
Marcos Amorim <markstm10@gmail.com>
2022-01-09 14:25:50 -0300

    fix: deleted hooks folder

--- src/Hooks/useAnimate.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-
-import { AnimateContext } from "../contexts/AnimateContext";
-
-export function useAnimate() {
-  const context = useContext(AnimateContext);
-
-  return context;
-}
--- src/Hooks/useTheme.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-
-import { ThemeContext } from "../contexts/ThemeContext";
-
-export function useTheme() {
-  const value = useContext(ThemeContext);
-
-  return value;
-}
