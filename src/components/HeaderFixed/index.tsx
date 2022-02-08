c32487ee2dc93dc2800d714eba84960c0259f582
Marcos Amorim <markstm10@gmail.com>
2022-02-07 22:34:51 -0300

    feat: seted hamb menu

--- src/components/HeaderFixed/index.tsx
+++ src/components/HeaderFixed/index.tsx
@@ -1,33 +1,43 @@
 import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 import { Wrapper } from "../Wrapper";
-import { Nav } from "./styles";
+import { HamburguerArea, Nav } from "./styles";
 
 const HeaderFixed = () => {
   return (
-    <Nav>
-      <li>
-        <a href="#home">
-          <Wrapper padding color="white">
-            Home
-          </Wrapper>
-        </a>
-      </li>
-      <li title="Em breve">
-        <a href="#about">
-          <Wrapper padding color="white">
-            Sobre mim
-          </Wrapper>
-        </a>
-      </li>
-      <li>
-        <a href="#projects">
-          <Wrapper padding color="white">
-            Projetos
-          </Wrapper>
-        </a>
-      </li>
-    </Nav>
+    <>
+      <HamburguerArea>
+        <Wrapper padding>
+          <GiHamburgerMenu fontSize="2rem" color="white" />
+        </Wrapper>
+      </HamburguerArea>
+      <Nav>
+        <div>
+          <li>
+            <a href="#home">
+              <Wrapper padding color="white">
+                Home
+              </Wrapper>
+            </a>
+          </li>
+          <li title="Em breve">
+            <a href="#about">
+              <Wrapper padding color="white">
+                Sobre mim
+              </Wrapper>
+            </a>
+          </li>
+          <li>
+            <a href="#projects">
+              <Wrapper padding color="white">
+                Projetos
+              </Wrapper>
+            </a>
+          </li>
+        </div>
+      </Nav>
+    </>
   );
 };
 
--- src/components/HeaderFixed/styles.ts
+++ src/components/HeaderFixed/styles.ts
@@ -10,22 +10,37 @@ export const Nav = styled.nav`
   z-index: 99;
   position: fixed;
 
-  li {
-    list-style: none;
+  div {
+    display: flex;
+    li {
+      list-style: none;
 
-    div {
-      transition: all ease 0.5s;
-      /* height: calc(100% - 10px); */
-      :hover {
-        filter: opacity(0.5);
-        border-bottom: 3px solid rgba(0, 0, 0, 0.5);
-        border-collapse: separate;
-        height: calc(100%);
+      div {
+        transition: all ease 0.5s;
+        /* height: calc(100% - 10px); */
+        :hover {
+          filter: opacity(0.5);
+          border-bottom: 3px solid rgba(0, 0, 0, 0.5);
+          border-collapse: separate;
+          height: calc(100%);
+        }
       }
     }
-  }
 
-  @media (max-width: 720px) {
+    @media (max-width: 720px) {
+      display: none;
+    }
+  }
+`;
+
+export const HamburguerArea = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  z-index: 999;
+
+  @media (min-width: 720px) {
     display: none;
   }
 `;
