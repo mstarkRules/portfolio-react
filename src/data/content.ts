93b27a2861c3d353559d716665453d3accbc2cac
Marcos Amorim <markstm10@gmail.com>
2022-02-09 12:41:44 -0300

    feat: created perfil content object

---
+++ src/data/content.ts
@@ -0,0 +1,8 @@
+export const content = {
+  name: "Marcos Paulo Amorim",
+  ocupation: "Desenvolvedor Front-end - ReactJS",
+  location: "Curitiba - PR",
+  about:
+    "Heyy <strong>(hoo)<strong>! Eu sou o Marcos, Desenvolvedor Frontend. Sou formado em Sistemas de Informação pelo Centro Universitário Luterano em Santarém, no Pará. Minha história com tecnologia começa em 2009, quando tive meus primeiros contatos com aquela máquina maravilhosa de 512 mb de memória, que rodava Fifa 07 no ultra (inclusive sdds de um Fifinha)! Em 2014 comecei a trabalhar com informática, e hoje desenvolvo aplicações web, utilizando principalmente o ReactJS e também Wordpress.",
+  email: "markstm10@gmail.com",
+};
