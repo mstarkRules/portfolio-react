356e6c75a60c8084e4f339b7c796fefda68cf7c9
Marcos Amorim <markstm10@gmail.com>
2022-02-11 00:36:39 -0300

    feat: seted arrows to slideItem

--- src/components/SlideItem/index.tsx
+++ src/components/SlideItem/index.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { PortfolioList } from "../../data/listPortfolio";
 import { Footer } from "../Footer";
 import { SocialBar } from "../SocialBar";
 import { SocialItem } from "../SocialItem";
+
+import { BiLeftArrow, BiRightArrow } from "react-icons/bi";
 import { Wrapper } from "../Wrapper";
 
-import { Container, SlideFooter } from "./styles";
+import { Container, SlideFooter, Arrow, ImgList } from "./styles";
+
+import perfil from "../../assets/mp.jpg";
 
 interface SlideItemProps {
   listImgs: ListImgsProps[];
@@ -20,14 +24,48 @@ interface ListImgsProps {
 }
 
 export function SlideItem({ listImgs, link, title }: SlideItemProps) {
+  const [scrollX, setScrollX] = useState(50);
+
+  function moveLeft() {
+    let x = scrollX + Math.round(window.innerWidth / 2);
+    if (x > 0) {
+      x = 0;
+    }
+    setScrollX(x);
+  }
+
+  function moveRight() {
+    let x = scrollX - Math.round(window.innerWidth / 2);
+    let listW = 900 * 3;
+
+    if (window.innerWidth - listW > x) {
+      x = window.innerWidth - listW - 200;
+    }
+    setScrollX(x);
+  }
   return (
     <Container>
       <Wrapper fontSize="big" strong padding>
         {title}
       </Wrapper>
-      <Wrapper>
-        <img src={listImgs[2].item} alt="image" title="Ir para o link" />
-      </Wrapper>
+      <ImgList>
+        <ul>
+          <li>
+            <div>
+              <img src={listImgs[2].item} alt="image" title="Ir para o link" />
+            </div>
+          </li>
+        </ul>
+
+        <Arrow>
+          <div onClick={moveLeft}>
+            <BiLeftArrow />
+          </div>
+          <div onClick={moveRight}>
+            <BiRightArrow />
+          </div>
+        </Arrow>
+      </ImgList>
 
       <SlideFooter>
         <SocialItem
--- src/components/SlideItem/styles.ts
+++ src/components/SlideItem/styles.ts
@@ -2,20 +2,43 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   height: auto;
-  img {
-    margin: 0;
-    padding: 1rem;
-    width: 100%;
-    /* max-width: 90%; */
-    max-height: calc(100vh - 13rem);
+`;
 
-    object-fit: cover;
+export const ImgList = styled.div`
+  ul {
+    list-style: none;
 
-    @media (max-width: 540px) {
-      padding: 0;
+    img {
+      margin: 0;
+      padding: 1rem;
+      width: 100%;
+
+      max-height: calc(100vh - 13rem);
+
+      object-fit: cover;
+
+      @media (max-width: 540px) {
+        padding: 0;
+      }
     }
   }
 `;
+
+export const Arrow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  position: absolute;
+  top: 50%;
+  width: 100%;
+  opacity: 0.5;
+
+  transition: all ease 0.3s;
+  :hover {
+    /* cursor: pointer; */
+    opacity: 0.5;
+  }
+`;
+
 export const SlideFooter = styled.footer`
   display: flex;
   flex-direction: row;
